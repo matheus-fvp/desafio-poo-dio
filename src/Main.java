78e0b90fd93e118d22e56dd92b8fd6d11b0f188d
Matheus Fernando Vieira Pinto <vieira.matheusf13@gmail.com>
2023-02-16 17:07:04 -0300

    add Curso.java and Mentoria.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import edu.matheusfvp.desafiopoo.dominio.Curso;
+import edu.matheusfvp.desafiopoo.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso = new Curso();
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("descrição curso java");
+        curso.setCargaHoraria(10);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("descrição curso python");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria("Java mentoria", "descrição mentoria de Java", LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
+
 }
